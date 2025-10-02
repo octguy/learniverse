9d8e978c8b9450f0e8ce0dfc75a8ce548cc4846e
OctGuy <heyiamthinh1003@gmail.com>
2025-10-02 23:02:06 +0700

    first ever commit

---
+++ learniverse-be/src/main/java/org/example/learniversebe/LearniverseBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.learniversebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearniverseBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearniverseBeApplication.class, args);
+	}
+
+}
---
+++ learniverse-be/src/test/java/org/example/learniversebe/LearniverseBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.learniversebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearniverseBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
