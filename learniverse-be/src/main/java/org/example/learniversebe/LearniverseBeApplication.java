cae442e85c0f7d8afdcb1bcd0a21c6856c17f8c7
OctGuy <heyiamthinh1003@gmail.com>
2025-10-15 16:25:41 +0700

    feat(be): implement user cleanup service for unverified users and their credentials

--- learniverse-be/src/main/java/org/example/learniversebe/LearniverseBeApplication.java
+++ learniverse-be/src/main/java/org/example/learniversebe/LearniverseBeApplication.java
@@ -2,8 +2,10 @@ package org.example.learniversebe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class LearniverseBeApplication {
 
 	public static void main(String[] args) {
--- learniverse-be/src/main/java/org/example/learniversebe/repository/UserRepository.java
+++ learniverse-be/src/main/java/org/example/learniversebe/repository/UserRepository.java
@@ -2,16 +2,18 @@ package org.example.learniversebe.repository;
 
 import org.example.learniversebe.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
-    Optional<User> findByUsername(String username);
-
     Optional<User> findByEmail(String email);
 
     Boolean existsByEmail(String email);
 
+    @Query(value = "select * from \"user\" where status = 'PENDING_VERIFICATION' and created_at + INTERVAL '24 hours' <= NOW();", nativeQuery = true)
+    List<User> findPendingUserExceedOneDay();
 }
